81ad1671ae38ca3292fd977d52f815394d4f4616
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-01-18 17:43:32 +0100

    create video converter

--- apps/example/src/app/app.component.ts
+++ apps/example/src/app/app.component.ts
@@ -1,52 +1,109 @@
-import { Component } from '@angular/core';
-
-interface AnimationFile {
-  name: string;
-  animations: AnimationState[];
-}
-interface AnimationState {
-  name: string;
-  playing: boolean;
-  time: number;
-  speed: number;
-  start: number;
-  end: number;
-  mix: number;
-  mode?: 'loop' | 'ping-pong' | 'one-shot';
+import { ChangeDetectorRef, Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { Artboard, RiveCanvasDirective } from 'ng-rive';
+import type { MediaRecorder } from './type-media-recorder';
+interface CanvasElement extends HTMLCanvasElement {
+  captureStream(frameRate?: number): MediaStream;
 }
 
-function createAnimation(name: string, start: number = 0, end?: number) {
-  return {
-    name,
-    start,
-    end,
-    playing: false,
-    time: start,
-    speed: 1,
-    mix: 0.5,
-  }
+const mimeTypes = {
+  'video/webm': 'video/webm;codecs=H264',
 }
-
 @Component({
   selector: 'ng-rive-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  trackByName = (i, item) => item.name; 
-  files: AnimationFile[] = [{
-    name: 'files',
-    animations: [
-      createAnimation('idle', 0.5, 4.5),
-      createAnimation('rollover_in'),
-      createAnimation('rollover_out')
-    ]
-  }, {
-    name: 'knight',
-    animations: [
-      createAnimation('idle', 0.9, 7),
-      createAnimation('day_night'),
-      createAnimation('night_day')
-    ]
-  }];
+  @ViewChild('download') download: TemplateRef<any>;
+  @ViewChild(RiveCanvasDirective) rivCanvas: RiveCanvasDirective;
+  animations: number[] = [];
+  file?: File;
+  snackRef: MatSnackBarRef<any>;
+  recorder: MediaRecorder;
+  recording = false;
+  dragging = false;
+  downloadUrl?: SafeUrl;
+
+  form = new FormGroup({
+    canvas: new FormGroup({
+      width: new FormControl(500),
+      height: new FormControl(500),
+    }),
+    output: new FormGroup({
+      name: new FormControl('rive'),
+      extension: new FormControl('video/webm'),
+    }),
+    animations: new FormControl([]),
+  });
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private snackbar: MatSnackBar,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  get canvas() {
+    return this.form.get('canvas')?.value;
+  }
+
+  get filename() {
+    const { name, extension } = this.form.get('output').value;
+    return `${name}.${extension.split('/').pop()}`;
+  }
+
+  upload(event: DragEvent) {
+    const input = event.target as HTMLInputElement;
+    this.file = input.files?.item(0);
+  }
+
+  setArtboard(artboard: Artboard) {
+    this.form.get('animations').reset();
+    this.animations = new Array(artboard.animationCount()).fill(null).map((_, i) => i);
+    console.log('animations', this.animations);
+    this.cdr.markForCheck();
+  }
+
+  async record() {
+    if (!('MediaRecorder' in window)) return;
+    this.revokeUrl();
+    this.recording = true;
+    const type = this.form.get('output.extension').value;
+    const mimeType = mimeTypes[type];
+    const recordedChunks = [];
+    const url: string = await new Promise((res, rej) => {
+      const stream = (this.rivCanvas.canvas as CanvasElement).captureStream(60 /*fps*/);
+      this.recorder = new window['MediaRecorder'](stream, { mimeType });
+
+      this.recorder.start();
+
+      this.recorder.ondataavailable = (e) => {
+        recordedChunks.push(e.data);
+        // after stop data avilable event run one more time
+        if (this.recorder.state === 'recording') {
+          this.recorder.stop();
+        }
+      }
+
+      this.recorder.onstop = (e) => {
+        const blob = new Blob(recordedChunks, { type });
+        const url = URL.createObjectURL(blob);
+        res(url);
+      }
+    });
+    this.downloadUrl = this.sanitizer.bypassSecurityTrustUrl(url);
+    this.snackRef = this.snackbar.openFromTemplate(this.download);
+  }
+
+  stopRecording() {
+    this.recorder?.stop();
+    this.recording = false;
+  }
+
+  revokeUrl() {
+    window.URL.revokeObjectURL(this.downloadUrl as string);
+    delete this.downloadUrl;
+  }
 }
--- apps/example/src/app/app.module.ts
+++ apps/example/src/app/app.module.ts
@@ -11,18 +11,28 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RiveModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatSliderModule,
     MatIconModule,
     MatButtonModule,
     MatListModule,
     MatSelectModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatSnackBarModule,
   ],
   bootstrap: [AppComponent],
 })
