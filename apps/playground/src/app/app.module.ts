60c040ba02fa1eb921921795f302fbdb168117da
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-01-31 17:44:52 +0100

    implement examle in playground

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-// import { RiveCanvas } from './canvas.directive';
 import { RiveModule } from 'ng-rive';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { SheepComponent } from './sheep/sheep.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // RiveCanvas
+    SheepComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RiveModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    RouterModule.forRoot([{
+      path: '',
+      redirectTo: 'sheep',
+      pathMatch: 'full'
+    }, {
+      path: 'sheep',
+      component: SheepComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent],
