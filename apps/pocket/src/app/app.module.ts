c0052ac0684e30952f3df0358db7531f7b8e3692
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-03-19 21:30:50 +0100

    setup firebase

--- apps/pocket/src/app/app.module.ts
+++ apps/pocket/src/app/app.module.ts
@@ -2,17 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule, USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/auth';
+import { AngularFirestoreModule, USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
+
+// Specific config for emulators
+const FIREBASE_EMUTLATORS = environment.useEmulator ? [
+  { provide: USE_AUTH_EMULATOR, useValue: ['localhost', 9099] },
+  { provide: USE_FIRESTORE_EMULATOR, useValue: ['localhost', 8080] },
+] : [];
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    AngularFireModule.initializeApp(environment.firebase, 'rive-pocket'),
+    AngularFirestoreModule.enablePersistence({ synchronizeTabs: true }),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    RouterModule.forRoot([{
+
+    }], {
+      initialNavigation: 'enabled',
+      paramsInheritanceStrategy: 'always',
+      relativeLinkResolution: 'corrected',
+      preloadingStrategy: PreloadAllModules
+    }),
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [...FIREBASE_EMUTLATORS],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
