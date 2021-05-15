2da921cfe2e3f0b0ad0a2b8473fafd12f70e9d9a
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2021-05-15 20:11:22 +0200

    update to nx 12.3.0 & ng 12.0.0

--- libs/ng-rive/src/lib/component/node.ts
+++ libs/ng-rive/src/lib/component/node.ts
@@ -2,6 +2,7 @@ import { Directive, Input, NgZone } from '@angular/core';
 import { RiveCanvasDirective } from '../canvas';
 import { RiveTransformComponent } from './transform-component';
 import { Node } from 'rive-canvas';
+import { Subscription } from 'rxjs';
 
 
 @Directive({
@@ -9,14 +10,21 @@ import { Node } from 'rive-canvas';
   exportAs: 'rivNode'
 })
 export class RiveNode extends RiveTransformComponent<Node> {
-  @Input() set x(value: number | string | null | undefined) {
+  @Input()
+  set x(value: number | string | null | undefined) {
     this.set('x', value);
   }
-
-  @Input() set y(value: number | string | null | undefined) {
-    this.set('y', value);
+  get x() {
+    return this.component?.x;
   }
 
+  @Input()
+  set y(value: number | string | null | undefined) {
+    this.set('y', value);
+  }
+  get y() {
+    return this.component?.y;
+  }
 
   constructor(zone: NgZone, canvas: RiveCanvasDirective) {
     super(zone, canvas);
