301889f67051f9138cf85c2f5b77f1b3bc6d393b
Kai Hentschel <kai@umami-creative.de>
2023-04-03 17:38:02 +0200

    Update service.ts
    local wasm path

--- libs/ng-rive/src/lib/service.ts
+++ libs/ng-rive/src/lib/service.ts
@@ -4,7 +4,7 @@ import { Inject, Injectable, Optional } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { animationFrame } from './frame';
 import { share } from 'rxjs/operators';
-import { RIVE_FOLDER, RIVE_VERSION } from './tokens';
+import { RIVE_FOLDER, RIVE_VERSION, RIVE_WASM } from './tokens';
 import { firstValueFrom, Observable } from 'rxjs';
 
 @Injectable()
@@ -17,15 +17,17 @@ export class RiveService {
   constructor(
     @Optional() @Inject(RIVE_FOLDER) private folder: string,
     @Optional() @Inject(RIVE_VERSION) private version: string,
+    @Optional() @Inject(RIVE_WASM) private localwasmPath: string,
     private http: HttpClient
   ) {
     this.folder = folder ?? 'assets/rive';
     this.version = version ?? 'latest';
+    this.wasmPath = localwasmPath ?? `https://unpkg.com/@rive-app/canvas-advanced@${this.version}/rive.wasm`;
   }
 
   private async getRive() {
     if (!this.rive) {
-      const locateFile = () => `https://unpkg.com/@rive-app/canvas-advanced@${this.version}/rive.wasm`;
+      const locateFile = () => this.wasmPath;
       this.rive = await Rive({ locateFile });
       this.frame = animationFrame(this.rive).pipe(share());
     }
